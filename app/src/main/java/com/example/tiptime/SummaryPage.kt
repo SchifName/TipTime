0ebf37ee8dc7acc672133e8794ac6363f36b0c44
Vincenzo <vincenzoangelino46@gmail.com>
2022-10-17 16:16:22 +0200

    xml summary page added

--- app/src/main/java/com/example/tiptime/SummaryPage.kt
+++ app/src/main/java/com/example/tiptime/SummaryPage.kt
@@ -1,5 +1,22 @@
 package com.example.tiptime
 
-class SummaryPage {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import com.example.tiptime.databinding.SummaryPageBinding
 
+class SummaryPage : AppCompatActivity(){
+
+    private lateinit var binding: SummaryPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SummaryPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.roundUp.text = ""
+        binding.tipResult.text =""
+        binding.total.text = ""
+        binding.backButton.setOnClickListener {  }
+    }
 }
